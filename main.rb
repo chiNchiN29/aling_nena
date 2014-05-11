4044725f04bfdb95eb5b2f0d6d7704aa4c716f4a
chiNchiN29 <chinchin_chua29@gmail.com>
2014-05-11 21:34:17 +0800

    new

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require './boot.rb'
+require './money_calculator.rb'
 
 # ROUTES FOR ADMIN SECTION
 get '/admin' do
@@ -43,3 +44,50 @@ get '/delete_product/:id' do
   redirect to '/admin'
 end
 # ROUTES FOR ADMIN SECTION
+
+get '/' do
+	@products = Item.all
+	@random = []
+	@random =@products.sample(10)
+	erb :home
+end
+
+get '/about.erb' do
+	erb :about 
+end
+
+get '/products.erb' do
+	@products = Item.all
+	erb :products
+end
+
+get '/buyform.erb/:id' do
+	@product = Item.find(params[:id])
+	erb :buyform
+end
+
+post '/thanks.erb/:id' do
+	@product = Item.find(params[:id])
+	alert = ""
+	@qtypurchased = params[:qty]
+	@price = @qtypurchased.to_i*@product.price.to_i
+	if @qtypurchased.to_i > @product.quantity.to_i
+		alert = "Not enough stock"
+		
+	else
+		@payment = MoneyCalculator.new params[:ones], params[:fives], params[:tens], params[:twenties], params[:fifties], params[:hundreds], params[:five_hundreds], params[:thousands]
+		@received = @payment.totalpay
+		@sukli = @payment.change(@price)
+		
+		if @price.to_i > @received.to_i
+			alert = "Not enough money"
+	
+		else
+			@give = @received.to_i - @price.to_i
+			@product.update_attributes!(
+			quantity: @product.quantity.to_i-@qtypurchased.to_i, sold: @product.sold.to_i+@qtypurchased.to_i
+			)	
+			erb :thanks
+		end
+	end
+end
--- money_calculator.rb
+++ money_calculator.rb
@@ -1,12 +1,70 @@
 class MoneyCalculator
+ 	
+	def initialize(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands)
+		@ones = ones	
+		@fives = fives
+		@tens = tens
+		@twenties = twenties
+		@fifties = fifties
+		@hundreds = hundreds
+		@five_hundreds = five_hundreds
+		@thousands = thousands	
 
-  def initialize(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands)
-    # each parameter represents the quantity per denomination of money
-    # these parameters can be assigned to instance variables and used for computation
-
-    # add a method called 'change' that takes in a parameter of how much an item costs
-    # and returns a hash of how much change is to be given
-    # the hash will use the denominations as keys and the amount per denomination as values
-    # i.e. {:fives => 1, fifties => 1, :hundreds => 3}
-  end
+		@return = Hash.new
+		@return["rthousands"] = 0
+		@return["rfive_hundreds"] = 0
+		@return["rhundreds"] = 0
+		@return["rfifties"] = 0
+		@return["rtwenties"] = 0
+		@return["rtens"] = 0
+		@return["rfives"] = 0
+		@return["rones"] = 0
+	end
+	
+	def totalpay
+		@totalpay = @ones.to_i*1+@fives.to_i*5+@tens.to_i*10+@twenties.to_i*20+@fifties.to_i*50+@hundreds.to_i*100+@five_hundreds.to_i*500+@thousands.to_i*1000
+		return @totalpay
+	end
+	
+	def change (price)
+		r2thousands = 0
+		r2five_hundreds = 0
+		r2hundreds = 0
+		r2fifties = 0
+		r2twenties = 0
+		r2tens = 0
+		r2fives = 0
+		r2ones = 0
+		@change = @totalpay.to_i-price.to_i
+		change = @change
+		until change == 0 do
+			if change >= 1000
+				r2thousands += 1
+				change -= 1000
+			elsif change >= 500
+				r2five_hundreds += 1
+				change -= 500
+			elsif change >= 100
+				r2hundreds += 1
+				change -= 100
+			elsif change >= 50
+				r2fifties += 1
+				change -= 50
+			elsif change >= 20
+				r2twenties += 1
+				change = change - 20
+			elsif change >= 10
+				r2tens += 1		
+				change -= 10
+			elsif change >= 5
+				r2fives += 1	
+				change -= 5
+			elsif @change >= 1
+				r2ones += 1
+				change -= 1
+			end
+		end
+		return {:rones => r2ones, :rfives => r2fives, :rtens => r2tens, :rtwenties => r2twenties, :rfifties => r2fifties, :rhundreds => r2hundreds, :rfive_hundreds => r2five_hundreds, :rthousands => r2thousands}
+		
+	end
 end
